202d6556819c608452d9f05c265d5687ff29491e
andzhe11a <andjella@abv.bg>
2025-06-10 14:11:25 +0300

    Update Prod.h

--- test_prep/task22/Prod.h
+++ test_prep/task22/Prod.h
@@ -3,14 +3,14 @@
 
 template <typename T>
 class Prod : public MathExpression<T> {
-private:
-    MathExpression<T>* left;
-    MathExpression<T>* right;
-
 public:
     Prod(MathExpression<T>* l, MathExpression<T>* r);
     ~Prod() override;
 
     T value() const override;
     void print() const override;
+
+private:
+    MathExpression<T>* left;
+    MathExpression<T>* right;
 };
