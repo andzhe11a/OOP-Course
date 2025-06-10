c0d31908520ede319bee8e94d5dcd01509a57665
andzhe11a <andjella@abv.bg>
2025-06-10 14:11:05 +0300

    Update Sum.h

--- test_prep/task22/Sum.h
+++ test_prep/task22/Sum.h
@@ -3,14 +3,14 @@
 
 template <typename T>
 class Sum : public MathExpression<T> {
-private:
-    MathExpression<T>* left;
-    MathExpression<T>* right;
-
 public:
     Sum(MathExpression<T>* l, MathExpression<T>* r);
     ~Sum() override;
 
     T value() const override;
     void print() const override;
+
+private:
+    MathExpression<T>* left;
+    MathExpression<T>* right;
 };
