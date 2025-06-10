3a31d87a6984394a3d149f9ec82f54a60dafdd36
andzhe11a <andjella@abv.bg>
2025-06-10 14:27:28 +0300

    Update main.cpp

--- test_prep/task22/main.cpp
+++ test_prep/task22/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "Constant.h"
 #include "Sum.h"
 #include "Prod.h"
@@ -6,41 +5,40 @@
 #include "Average.h"
 
 int main() {
-    using std::cout;
-    using std::endl;
-
     MathExpression<int>* c1 = new Constant<int>(3);
     MathExpression<int>* c2 = new Constant<int>(7);
     MathExpression<int>* c3 = new Constant<int>(10);
-
-    MathExpression<int>* sum = new Sum<int>(c1, c2);
-    cout << "Sum: ";
-    sum->print();
-    cout << " = " << sum->value() << endl;
-
-    MathExpression<int>* prod = new Prod<int>(sum, c3);
-    cout << "Prod: ";
-    prod->print();
-    cout << " = " << prod->value() << endl;
 
     Max<int> maxExpr;
     maxExpr.add(new Constant<int>(2));
     maxExpr.add(new Constant<int>(8));
     maxExpr.add(new Constant<int>(6));
-    cout << "Max: ";
+    std::cout << "Max: ";
     maxExpr.print();
-    cout << " = " << maxExpr.value() << endl;
+    std::cout << " = " << maxExpr.value() << std::endl;
 
     Average<int> avgExpr;
     avgExpr.add(new Constant<int>(5));
     avgExpr.add(new Constant<int>(15));
     avgExpr.add(new Constant<int>(25));
-    cout << "Average: ";
+    std::cout << "Average: ";
     avgExpr.print();
-    cout << " = " << avgExpr.value() << endl;
+    std::cout << " = " << avgExpr.value() << std::endl;
+
+    MathExpression<int>* sum = new Sum<int>(c1, c2);
+    std::cout << "Sum: ";
+    sum->print();
+    std::cout << " = " << sum->value() << std::endl;
+
+    MathExpression<int>* prod = new Prod<int>(c1, c3);
+    std::cout << "Prod: ";
+    prod->print();
+    std::cout << " = " << prod->value() << std::endl;
 
     delete prod;
     delete sum;
+    delete c1;
+    delete c2;
     delete c3;
 
     return 0;
