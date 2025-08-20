cd8e615945d2e448e3b2a7e800a4b8222dc59231
andzhe11a <andjella@abv.bg>
2025-08-20 17:03:26 +0300

    Update main20.cpp

--- Other/short_task/main20.cpp
+++ Other/short_task/main20.cpp
@@ -9,9 +9,9 @@ int main() {
     myStore.printStatus();
 
     std::cout << "\nAdding new phones: " << std::endl;
-    myStore.addPhone(new Smartphone("Galaxy S5", "Samsung", 1800, "Android 15", 256));
-    myStore.addPhone(new Smartphone("Galaxy A5", "Samsung", 800, "Android 15", 128));
-    myStore.addPhone(new Smartphone("15", "Apple", 9500, "iOS 15", 256));
+    myStore.addPhone(Smartphone("Galaxy S5", "Samsung", 1800, "Android 15", 256));
+    myStore.addPhone(Smartphone("Galaxy A5", "Samsung", 800, "Android 15", 128));
+    myStore.addPhone(Smartphone("15", "Apple", 9500, "iOS 15", 256));
 
     myStore.printStatus();
 
